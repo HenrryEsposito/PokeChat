964d085d6529bce8bac6fed8fab30ddcdbc00d69
Henrry Esposito <henrrygesposito@gmail.com>
2022-05-14 16:20:02 -0300

    style - set global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,35 @@
+import { createStyles, makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    '@global': {
+      html: {
+        '-webkit-font-smoothing': 'antialiased',
+        '-moz-osx-font-smoothing': 'grayscale',
+        height: '100%',
+        width: '100%'
+      },
+      body: {
+        height: '100%',
+        width: '100%',
+        margin: '0',
+        padding: '0'
+
+      },
+      '#root': {
+        height: '100%',
+        width: '100%',
+        margin: '0',
+        padding: '0'
+      }
+    }
+  })
+)
+
+const GlobalStyles = () => {
+  useStyles()
+
+  return null
+}
+
+export default GlobalStyles
